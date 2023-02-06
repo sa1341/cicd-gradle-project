a6d89e84692aef62429e0bf7985d93ef7b42e9ca
임준영 <a79007714@gmail.com>
2023-02-06 22:49:22 +0900

    ktlin pre-commit hook test

--- src/test/kotlin/com/junyoung/cicdgradleproject/product/service/FundProductBuyingTest.kt
+++ src/test/kotlin/com/junyoung/cicdgradleproject/product/service/FundProductBuyingTest.kt
@@ -38,6 +38,11 @@ class FundProductBuyingTest {
     }
 
     companion object {
-        val fundBuyingReq = FundBuyingReq(accountNumber = "02000162758",  fundCod = "2006001",  name = "임준영",  age = 32)
+        val fundBuyingReq = FundBuyingReq(
+            accountNumber = "02000162758",
+            fundCod = "2006001",
+            name = "임준영",
+            age = 32
+        )
     }
 }
